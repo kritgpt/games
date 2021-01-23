19277d93ff25f77c384ae1a048a74f350228e55e
kritgpt <68438410+kritgpt@users.noreply.github.com>
2021-01-23 18:33:52 +0530

    Just improved formatting

--- TicTacPy.py
+++ TicTacPy.py
@@ -1,37 +1,19 @@
-#TicTacPy
-
 #initial board has position numbers to introduce to user
 board = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 #number of players
 pnum = 2
 
-import sys
-
-
 #function to show board
 def printBoard(brd):
     print()
     print(
-        brd[0],
-        ' | ',
-        brd[1],
-        ' | ',
-        brd[2],
+        brd[0],' | ',brd[1],' | ',brd[2],
         "\n---------\n",
-        brd[3],
-        ' | ',
-        brd[4],
-        ' | ',
-        brd[5],
+        brd[3],' | ',brd[4],' | ',brd[5],
         "\n---------\n",
-        brd[6],
-        ' | ',
-        brd[7],
-        ' | ',
-        brd[8],
+        brd[6],' | ',brd[7], ' | ',brd[8],
         sep='')
-
-
+    
 #function to clear out board
 def bClear():
     for q in range(len(board)):
@@ -59,49 +41,39 @@ class player:
 def pTurn(k):
     while True:
         print(l[k].nm, ", enter box number ", end='')
-        try:
-            pPos = int(input("")) - 1
+        # try:
+        pPos = int(input("")) - 1
 
-            if board[pPos] != ' ':
-                print(
-                    "\n\tInvalid turn. Box is already filled. Pick another box.\n")
-                continue
-            else:
-                board[pPos] = l[k].symb
-                fWin(k)
-                break
-        except:
-            print("\n\tPlease enter a valid position number\n")
+        if board[pPos] != ' ':
+            print(
+                "\n\tInvalid turn. Box is already filled. Pick another box.\n")
             continue
+        else:
+            board[pPos] = l[k].symb
+            fWin(k)
+            break
+        except:
+             print("\n\tPlease enter a valid position number\n")
+             continue
 
     printBoard(board)
 
-
 #-----------------------winning code-----------------------#
 winState = False
-tieState = False
+
 
 def fWin(k):
-    global winState
-    global tieState
     for i in range(0, 9, 3):
         j = int(i / 3)
         c1 = board[i] != ' ' and board[i] == board[i + 1] and board[
             i + 1] == board[i + 2]
         c2 = board[j] != ' ' and board[j + 3] == board[j] == board[j + 6]
         c3 = board[4] != ' ' and (board[0] == board[4] == board[8]
-                                  or board[2] == board[4] == board[6])
+                                  or board[2] == board[4] == board[6])  #✨I didn't really want to specify the exact index numbers so that I can maybe extend the grid to 4*4 or 5*5 in the future (based on user preference), but I also didn't know how else to do this
         if c1 == True or c2 == True or c3 == True:
             print('\n\n\n\t--------you won, ', l[k].nm, '--------')
+            global winState
             winState = True
-            break
-    c=0
-    for i in range(9):
-        if board[i]!=' ':
-            c+=1
-        if c==9 and winState!=True:
-            print('\n\n\n\t--------The game tied--------')
-            tieState = True
             break
 
 
@@ -120,13 +92,12 @@ for i in range(pnum):
     l[i] = player(nme, 0, sym[i])
 
 bClear()
-
 #game starts
 print("\nWelcome to TicTacPy! \n")
 
 k = 0
 while True:
-    if winState == True or tieState == True:
+    if winState == True:        #✨ should I instead do a thing where it just switched to the next symbol after each move instead of all this?
         break
     else:
         pTurn(k)
